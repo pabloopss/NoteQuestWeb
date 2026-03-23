78dfd819519da02615a909cecb9ea04eca0b2474
EmersonDevLucas <emersonlucasts@gmail.com>
2026-03-23 16:49:36 -0300

    login issues

--- firebase.js
+++ firebase.js
@@ -1,10 +1,11 @@
 // firebase.js - O Coração do Jogo
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-auth.js";
-import { getFirestore, doc, setDoc, getDoc, updateDoc, onSnapshot, collection, getDocs, deleteDoc } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
+// Repara que adicionei o getDocFromServer na linha abaixo:
+import { getFirestore, doc, setDoc, getDoc, getDocFromServer, updateDoc, onSnapshot, collection, getDocs, deleteDoc } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAVNJpud6rhUHEWZxBSJ-o-8WcMtexk6yw", // Confirme se é essa mesma!
+    apiKey: "AIzaSyAVNJpud6rhUHEWZxBSJ-o-8WcMtexk6yw",
     authDomain: "notequestweb.firebaseapp.com",
     projectId: "notequestweb",
     storageBucket: "notequestweb.firebasestorage.app",
@@ -17,4 +18,5 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
 
-export { auth, db, googleProvider, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged, signInWithPopup, doc, setDoc, getDoc, updateDoc, onSnapshot, collection, getDocs, deleteDoc };
+// Adiciona o getDocFromServer aqui no final também:
+export { auth, db, googleProvider, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged, signInWithPopup, doc, setDoc, getDoc, getDocFromServer, updateDoc, onSnapshot, collection, getDocs, deleteDoc };
